57331a6e430b8de7693d4a19fc4749b6e10f4ae7
Rüdiger Wolff <rudi.wolff@yahoo.com>
2024-03-18 15:24:27 +0100

    Zweite Version des Backup-Skriptes, das für alle User das %HOME-Verzeichnis sichert.

--- ASIR_M06B2_EAC4_scripts/backup.sh
+++ ASIR_M06B2_EAC4_scripts/backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # ASX M06B2 EAC4
-# Shell script en Bash per fer còpies de seguretat / sincronització de directoris del sistema
+# Descripció: Shell script en Bash per fer còpies de seguretat / sincronització 
+#             de directoris del sistema
 # Autor: Rüdiger Wolff
-# Data: 24/11/2023
+# Data: 5/12/2023
+#  
 
 ########  Definicions de variables  ########
 backup_path=/backups    # Ruta de la carpeta de còpia de seguretat
@@ -11,82 +13,82 @@ retencio=$1             # Els fitxers de còpia de seguretat no poden tenir més
 
 # Codis d'error:
 ERR_ROOT=1  # L'usuari no és root
-ERR_ARG=2   # Arguments <> 2 o mes
-ERR_RET=3   # Primer argument fora de rang
-ERR_TAR=4   # El programa tar no està instal·lat al sistema
-ERR_UEX=5   # Usuari no existeix
+ERR_ARG=2   # Arguments no son 2 o mes
+ERR_TAR=3   # El programa tar no està instal·lat al sistema
+ERR_UEX=4   # Usuari no existeix
+ERR_BKU=5   # Ha fallat backup
 
 ########  Definicions de funcions  ########
 usage (){
-    echo >&2 "USAGE: $(basename $0) rentencions{0..5} login [login...]"
+    echo >&2 "USAGE: $(basename $0) rentencions login [login...]"
 }
 
 error_message(){
     case $1 in
         root)
-        echo >&2 "ERROR: Cal ser root per executar el programa"
-        usage
-        ;;
+            echo >&2 "ERROR: Cal ser root per executar el programa"
+            usage
+            exit $ERR_ROOT
+            ;;
         arguments)
-        echo >&2 "ERROR: el programa rep almenys 2 arguments"
-        usage
-        ;;
-        retencio)
-        echo >&2 "ERROR: El nombre de retencions està fora del rang permès."
-        usage
-        ;;
+            echo >&2 "ERROR: el programa rep almenys 2 arguments"
+            usage
+            exit $ERR_ARG
+            ;;
         no_tar)
-        echo 'El programa "tar" no existeix en el sistema. Sortint...'
-        ;;
+            echo >&2 'El programa "tar" no existeix en el sistema. Sortint...'
+            exit $ERR_TAR
+            ;;
         user)
-        echo >&2 "ERROR: login $2 inexistent"
-        usage
-        ;;
+            echo >&2 "ERROR: login $2 inexistent"
+            usage
+            ;;
+        backup)
+            echo >&2 "ERROR: ha fallat la còpia de seguretat de $2"
+            status=$ERR_BKU
+            ;;
     esac
 }
 
-# Comprova cada argument per veure si és un usuari existent i 
-# referència a funcions , si és el cas.
-user_exists(){
-    codi_return=0
-    until [ $# = 1 ];do
-        shift 1
-        if [ ! "$(grep "^$1:" /etc/passwd)" ];then
-            error_message user $1
-            codi_return=$ERR_UEX
-        else
-            remove_old_files $retencio $1
-            backup_home $retencio $1
-        fi
-    done <<< $*
-    return $codi_return
-}
-
 remove_old_files(){
-    find $backup_path -type f -mtime +$retencio -iname $2* -exec rm -rf {} \;
-    }
+    find $1 -type f -mtime +$2 -iname $3* -exec rm -rf {} \;
+}
 
 # Realitza la còpia de seguretat per a cada usuari amb tar amb format nom-aaaammdd.tgz
 backup_home(){
-    tar -czf $backup_path/$2-$(date +%Y%m%d).tgz /home/$2 >& /dev/null
+    home_dir=$(getent passwd $3 | cut -d: -f6)
+    tar -czf $1/$3-$(date +%Y%m%d).tgz $home_dir >& /dev/null || error_message backup $3
 }
 
 
 ########  Programari principal  ########
 
 # El programa valida que s’executi com a root.
-test $USER != root && error_message root && exit $ERR_ROOT
+test $EUID -ne 0 && error_message root
 
 # El programa valida que es reben almenys dos arguments.
-test $# -lt 2 && error_message arguments && exit $ERR_ARG
-
-# El programa valida que es reb un nombre entre 0 i 5
-test $1 -lt 0 -o $1 -gt 5 && error_message retencio && exit $ERR_RET
+test $# -lt 2 && error_message arguments 
 
 # El programa valida si está instalado el programa 'tar'. 
-test -z $(which tar) && error_message no_tar && exit $ERR_TAR
+test ! -x $(which tar) && error_message no_tar
 
 # Si el directori destí /backups no existeix, el crea.
 test ! -d $backup_path && mkdir $backup_path
 
-user_exists $*
+# Comprova cada argument per veure si és un usuari existent i 
+# referència a funcions , si és el cas.
+status=0
+until [ $# -eq 1 ];do
+        shift
+    if [ ! "$(grep "^$1:" /etc/passwd)" ];then
+        error_message user $1
+        test $status -eq $ERR_BKU || status=$ERR_UEX
+    else
+        remove_old_files $backup_path $retencio $1
+        backup_home $backup_path $retencio $1
+    fi
+done <<< $*
+exit $status
+
+
+
